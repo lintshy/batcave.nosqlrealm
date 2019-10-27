34f47df4598209d6a852a6d3b4189651299ef305
lintshy <govindj1991@gmail.com>
2019-10-27 10:09:54 -0700

    seperate mongo options

--- src/store/connector.ts
+++ src/store/connector.ts
@@ -1,16 +1,19 @@
 import { MongoClient } from 'mongodb'
 import mongoose from 'mongoose'
-
 require('dotenv').config()
+
+const options = { useNewUrlParser: true, useUnifiedTopology: true }
 const url = (process.env.MONGO_URL) ? process.env.MONGO_URL : ''
+
+
 export const getMongoConnection = async (): Promise<MongoClient> => {
 
-    const client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+    const client = await MongoClient.connect(url, options)
     return client
 }
 
 export const getMongooseConnection = async (): Promise<mongoose.Connection> => {
 
-    const mongooseConnection = await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+    const mongooseConnection = await mongoose.connect(url, options)
     return mongooseConnection.connection
 }
